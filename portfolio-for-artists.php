636c4ccf8ea246843662d9a8e75dad583e59e941
Jiří Maha <nezapomenout@gmail.com>
2020-05-07 16:40:54 +0200

    Rename project

--- portfolio-for-artists.php
+++ portfolio-for-artists.php
@@ -4,7 +4,7 @@ namespace Grav\Theme;
 use Grav\Common\Grav;
 use Grav\Common\Theme;
 
-class Portfolio extends Theme
+class PortfolioForArtists extends Theme
 {
     // Access plugin events in this class
 
