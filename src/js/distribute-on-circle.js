544bfba7090abfe4bd8e6b9044d25cfc21e529a9
Jiří Maha <nezapomenout@gmail.com>
2020-06-05 17:07:33 +0200

    Change hover style of intro item - calculate box-shadow in js

--- src/js/distribute-on-circle.js
+++ src/js/distribute-on-circle.js
@@ -7,6 +7,14 @@ const distributeOnCircle = ($elements, radius = 19, modifier = 0) => {
     const y = Math.sin(itemAngle) * radius;
 
     $element.style.transform = `translate(calc(-50% + ${x}vmin), calc(-50% + ${y}vmin))`;
+
+    $element.addEventListener('mouseenter', () => {
+      $element.style.boxShadow = `${-x * .125}vmin ${-y * .125}vmin ${1.625 * 2}vmin rgba(0,0,0,.5)`;
+    });
+
+    $element.addEventListener('mouseleave', () => {
+      $element.style.boxShadow = `0 0 0 rgba(0,0,0,0)`;
+    });
   });
 }
 
